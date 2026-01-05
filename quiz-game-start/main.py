f775c88821ebe08f5570cc62d2b3c522689ff4ae
Dwarfy02-pro <18191082.dpsgv@dpsgs.org>
2026-01-05 10:02:27 +0530

    Add files via upload

--- quiz-game-start/main.py
+++ quiz-game-start/main.py
@@ -3,9 +3,9 @@ from question_model import Question
 from quiz_brain import QuizBrain
 
 Question_bank =[]
-for i in range(0,12):
-    text = question_data[i].get("text")
-    answer = question_data[i].get("answer")
+for i in range(0,10):
+    text = question_data[i].get("question")
+    answer = question_data[i].get("correct_answer")
     new_q = Question(text=text,answer=answer)
     Question_bank.append(new_q)
 
